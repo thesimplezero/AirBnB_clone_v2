58293c1d6725a4a28238882a915d1c00f6161d00
thesimplezero <hi@thesimplezero.io>
2023-10-05 17:09:40 +0300

    setup web static - edit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,53 +1,31 @@
 #!/usr/bin/env bash
+# sets up my web servers for the deployment of web_static
 
-# Install Nginx if it not already installed
-if ! [ -x "$(command -v nginx)" ]; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get install -y nginx
 
-# Create the folder /data/ if it doesn’t already exist
-if ! [ -d "/data/" ]; then
-    sudo mkdir /data/
-fi
+# Create folders if not already exists
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-# Create the folder /data/web_static/ if it doesn’t already exist
-if ! [ -d "/data/web_static/" ]; then
-    sudo mkdir /data/web_static/
-fi
+# Create a fake HTML file for testing
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" >> /data/web_static/releases/test/index.html
 
-# Create the folder /data/web_static/releases/ if it doesn’t already exist
-if ! [ -d "/data/web_static/releases/" ]; then
-    sudo mkdir /data/web_static/releases/
-fi
+# Create & recreate a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create the folder /data/web_static/shared/ if it doesn’t already exist
-if ! [ -d "/data/web_static/shared/" ]; then
-    sudo mkdir /data/web_static/shared/
-fi
-
-# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
-if ! [ -d "/data/web_static/releases/test/" ]; then
-    sudo mkdir /data/web_static/releases/test/
-fi
-
-# Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
-# If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
-if [ -L "/data/web_static/current" ]; then
-    sudo rm /data/web_static/current
-fi
-
-sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist).
-# This should be recursive; everything inside should be created/owned by this user/group.
+# Give ownership to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static).
-# Don’t forget to restart Nginx after updating the configuration:
-sudo sed -i 's/server_name _;/server_name _;\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}/' /etc/nginx/sites-available/default
+# update Nginx config to serve /data/web_static/current at /hbnb_static/
+sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
+# Restart Nginx to apply changes
 sudo service nginx restart
