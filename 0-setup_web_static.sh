7cb6f82793bfb0c0efb2fdbc56d354afbd0247af
thesimplezero <hi@thesimplezero.io>
2023-10-05 17:34:14 +0300

    setup web static - modified

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,47 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Constants for directories and config file
-WEB_ROOT="/data/web_static"
-SHARED_DIR="$WEB_ROOT/shared"
-RELEASES_DIR="$WEB_ROOT/releases/test"
-NGINX_CONFIG_FILE="/etc/nginx/sites-available/default"
+# Install Nginx if not already installed and create necessary folders
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/{shared,releases/test}
 
-# Function to create a directory if it doesn't exist
-create_directory() {
-    if [ ! -d "$1" ]; then
-        sudo mkdir -p "$1"
-    fi
-}
-
-# Function to add or update Nginx configuration
-update_nginx_config() {
-    config_content="location /hbnb_static/ {
-        alias /data/web_static/current/;
-        index index.html;
-    }"
-
-    if grep -q "location /hbnb_static/" "$NGINX_CONFIG_FILE"; then
-        sudo sed -i "/location \/hbnb_static\//c\\$config_content" "$NGINX_CONFIG_FILE"
-    else
-        sudo sed -i "26i \\\t$config_content" "$NGINX_CONFIG_FILE"
-    fi
-}
-
-# Install Nginx if not already installed
-if ! dpkg -l | grep -q nginx; then
-    echo "Installing Nginx..."
-    sudo apt-get update
-    sudo apt-get install -y nginx
-fi
-
-# Create required directories
-echo "Creating directories if they don't exist..."
-create_directory "$SHARED_DIR"
-create_directory "$RELEASES_DIR"
-
-# Ensure proper ownership
-echo "Setting ownership..."
-sudo chown -R ubuntu:ubuntu "$WEB_ROOT"
 
 # Create a fake HTML file for testing
 echo "<html>
@@ -50,20 +13,16 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee "$RELEASES_DIR/index.html" >/dev/null
+</html>" >> /data/web_static/releases/test/index.html
 
-# Create and recreate a symbolic link
-echo "Creating or recreating symbolic link..."
-sudo ln -sf "$RELEASES_DIR" /data/web_static/current
+# Create & recreate a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Update Nginx configuration
-echo "Updating Nginx configuration..."
-update_nginx_config
+# Give ownership to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# update Nginx config to serve /data/web_static/current at /hbnb_static/
+sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
 # Restart Nginx to apply changes
-echo "Restarting Nginx..."
 sudo service nginx restart
-
-# Exit with success
-echo "Script execution completed successfully."
-exit 0
