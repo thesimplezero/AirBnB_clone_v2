80945f5f1acdad86b1820f328eb5f1b6b4bc6a8f
thesimplezero <hi@thesimplezero.io>
2023-10-08 17:02:04 +0300

    deploy to servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,54 +1,43 @@
 #!/usr/bin/python3
-"""
-This fabfile manages the distribution of web_static files to web servers.
-"""
+"""Fabric script to deploy web static archives."""
 
 import os
 from fabric.api import *
 from datetime import datetime
 
-# Set the host IP addresses for web-01 && web-02
-env.hosts = ['54.208.71.253', '100.25.144.89']
-env.user = "ubuntu"
+env.hosts = ['3.236.119.223', '54.234.28.162']
+env.user = 'ubuntu' 
 env.key_filename = '~/.ssh/id_rsa'
 
-
 def do_pack():
-    """Create a tar.gz archive of web_static."""
-
-    # Obtain the current date and time
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-
-    # Construct the path where the archive will be saved
-    archive_path = f"versions/web_static_{now}.tgz"
-
-    # Create the 'versions' directory if it doesn't exist
-    local("mkdir -p versions")
-
-    # Use the 'tar' command to create a compressed archive
-    result = local(f"tar -czvf {archive_path} web_static")
-
-    return archive_path if result.succeeded else None
-
+    """Generate a .tgz archive from web_static/."""
+    try:
+        local("mkdir -p versions")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        arch_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(arch_name))
+        return arch_name
+    except:
+        return None
 
 def do_deploy(archive_path):
-    """Deploy an archive to the web servers."""
+    """Distribute an archive to web servers."""
+    if not os.path.exists(archive_path):
+        return False
 
-    if os.path.exists(archive_path):
-        archive = archive_path.split('/')[1]
-        a_path = f"/tmp/{archive}"
-        folder = archive.split('.')[0]
-        f_path = f"/data/web_static/releases/{folder}/"
-
-        put(archive_path, a_path)
-        run(f"mkdir -p {f_path}")
-        run(f"tar -xzf {a_path} -C {f_path}")
-        run(f"rm {a_path}")
-        run(f"mv -f {f_path}web_static/* {f_path}")
-        run(f"rm -rf {f_path}web_static")
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}/".format(no_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_n, no_ext))
+        run("rm /tmp/{}".format(file_n))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(no_ext, no_ext))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(no_ext))
         run("rm -rf /data/web_static/current")
-        run(f"ln -s {f_path} /data/web_static/current")
-
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(no_ext))
+        print("New version deployed!")
         return True
-
-    return False
+    except:
+        return False
