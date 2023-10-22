35fa0f908ccc09b31bf12f30f10a915b1f824a87
thesimplezero <hi@thesimplezero.io>
2023-10-22 18:43:13 +0300

    add  public method def close(self)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,3 +62,7 @@ class FileStorage:
             key = obj.__class__.__name__+'.'+obj.id
             if key in self.__objects:
                 del self.__objects[key]
+
+    def close(self):
+        """Calls the reload method to deserialize the JSON file to objects."""
+        self.reload()
